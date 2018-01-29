3791041837c898d8a0af1dc4e73b9a30753f48e6
danielt <danielt@perfectomobile.com>
2018-01-30 00:01:26 +0200

    Add runtime table

--- chatWebApp/src/fileupload/FileUploadServlet.java
+++ chatWebApp/src/fileupload/FileUploadServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet("/pages/upload")
+@WebServlet("/upload")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5)
 public class FileUploadServlet extends HttpServlet {
 
